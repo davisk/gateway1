22170f799ac7e6cc0721609ac81960101bfeafce
Christian Murphy <Christian.Murphy@asu.edu>
2014-01-30 13:43:24 -0800

    adding installer script

---
+++ install/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#install required debian packages
+cat aptitude.txt | xargs sudo apt-get install --yes 
+
+#install required python packages
+sudo pip install -r requirements.txt
+
+#creates application database
+echo "create database gateway" | mysql --user=root --password=root
+
+#set up django
+cd ../gateway
+python manage.py syncdb
