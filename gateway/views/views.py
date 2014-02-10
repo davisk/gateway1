e39412fefd4e95d248b0830e05b820b6b66e011a
Christian Murphy <Christian.Murphy@asu.edu>
2014-02-09 18:31:41 -0800

    adding ventures page adding alert bootstrap mixin

--- gateway/views/views.py
+++ gateway/views/views.py
@@ -56,4 +56,26 @@ def profile_view(request):
     	return render(request, 'profile.html', dict(user=user))
 
 def ventures_view(request):
-    return render(request, 'ventures.html')
+	ventures = [
+		{
+			"image": "glyphicon-book",
+			"title": "Book Store",
+			"description": "Your chain of bookstores Borders isn't doing so well."
+		},
+		{
+			"image": "glyphicon-phone-alt",
+			"title": "Call Center",
+			"description": "Your help office has collapsed, killing all the workers. But you have collected 200% with insurance fraud! You're a real tycoon!"
+		},
+		{
+			"image": "glyphicon-music",
+			"title": "Music Business",
+			"description": "Your website Napster has been shutdown by U.S. authorities. Better luck next time."
+		}
+	]
+	currentVenture = {
+		"image": "glyphicon-briefcase",
+		"title": "Current Venture",
+		"description": "You're doing good! Keep it up!"
+	}
+    	return render(request, 'ventures.html', dict(ventures=ventures, currentVenture=currentVenture) )
