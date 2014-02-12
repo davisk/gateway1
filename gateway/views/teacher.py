4039b3a09cd9533968bfd9ab5f34220ba661d49f
sl8tr <bsleater@asu.edu>
2014-02-12 16:41:41 -0700

    fixed coding style of nav template

--- gateway/views/teacher.py
+++ gateway/views/teacher.py
@@ -3,19 +3,44 @@ from django.shortcuts import render
 
 
 def teacher_view(request):
-    profileTeacher = {
-        "id": "teacher-admin-content",
-        "left_title": "Profile",
-        "middle_title": "Manage",
-        "right_title": "Account",
-        "dd1_title": "Settings",
-        "dd2_title": "Help"
-    }
+    subnav = [
+        {
+            "id": "tabi-profile",
+            "title": "Profile",
+            "icon": "glyphicon-user",
+            "content": "test profile",
+            "active": "active"
+        },
+        {
+            "id": "tabi-manage",
+            "title": "Manage",
+            "icon": "glyphicon-tasks",
+            "content": "test manage"
+        },
+        {
+            "id": "tabi-account",
+            "title": "Account",
+            "icon": "glyphicon-cloud",
+            "content": "test account"
+        },
+        {
+            "id": "tabi-settings",
+            "title": "Settings",
+            "icon": "glyphicon-cog",
+            "content": "test settings"
+        },
+        {
+            "id": "tabi-help",
+            "title": "Help",
+            "icon": "glyphicon-question-sign",
+            "content": "test help"
+        }
+    ]
 
     return render(
         request,
         'teacher.html',
         dict(
-            profileTeacher=profileTeacher
+            subnav=subnav
         )
     )
