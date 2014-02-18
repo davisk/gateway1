e7b4e3869716affdf5c52daa8be74436fc8d49a2
JoeStratton <joe.stratton@asu.edu>
2014-02-18 15:23:07 -0700

    fixed the broken page load

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,7 +1,7 @@
 from community import community_view
 from home import home_view
 from members import members_view
-from minigames import minigames_view
+#from minigames import minigames_view
 from profile import profile_view
 from ventures import ventures_view
 from teacher import teacher_view
@@ -11,7 +11,7 @@ __all__ = [
     "home_view",
     "community_view",
     "members_view",
-    "minigames_view",
+    #"minigames_view",
     "profile_view",
     "ventures_view",
     "teacher_view"
