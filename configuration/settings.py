141b71998921c50adb749a3fb86fb477e394cec0
JoeStratton <joe.stratton@asu.edu>
2014-02-18 18:36:10 -0700

    faq page filler complete til content included

--- configuration/settings.py
+++ configuration/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'gateway',
         'USER': 'root',
-        'PASSWORD': 'nowaqigu',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '',
         'OPTIONS': {
