43904624c960075479f3146dbbbd5430a771392b
sl8tr <bsleater@asu.edu>
2014-03-03 12:54:49 -0700

    got confused on teacher, fixed url issue

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,7 +1,6 @@
 from community import community_view
 from home import home_view
 from profile import profile_view
-from teacher import teacher_view
 from ventures import ventures_view
 from faqs import faqs_view
 
@@ -9,7 +8,6 @@ __all__ = [
     "home_view",
     "community_view",
     "profile_view",
-    "teacher_view",
     "ventures_view",
     "faqs_view"
 ]
