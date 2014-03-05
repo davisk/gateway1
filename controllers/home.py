4b6b81743d89f596126b8fb76ce2219277c63f5d
Christian Murphy <christian.murphy@asu.edu>
2014-03-05 09:28:27 -0700

    new members home page

--- controllers/home.py
+++ controllers/home.py
@@ -4,33 +4,103 @@ from django.shortcuts import render
 
 def home_view(request):
     if request.user.is_authenticated():
-        news = [
+        ventures = [
             {
-                "title": "Happy New Year!",
-                "date": "1.1.2014"
+                "title": "Murphee's Irish Coats",
+                "image": "coat",
+                "progress": "90",
+                "step": "elevator",
+                "hook": "Bringing the comfort and quality of Traditional Irish\
+                        coats to an Online International Exchange",
+                "description": "Coming Soon!",
+                "id": "0"
             },
             {
-                "title": "Planet Express has made their first delivery!",
-                "date": "21.12.2013"
+                "title": "Three Lions Magic Show",
+                "image": "anti-gravity",
+                "progress": "60",
+                "step": "survey",
+                "hook": "A crew of three globally acclaimed Magicians bend the\
+                        rules of reality to make the mundane extraordinary",
+                "description": "Comming Soon!",
+                "id": "1"
             },
             {
-                "title": "Today in 1982 Bill Gates made his first dollar!",
-                "date": "14.12.2013"
+                "title": "Melies Studio",
+                "image": "film",
+                "progress": "70",
+                "step": "interest",
+                "hook": "Georges Melies makes Black and White cinematography \
+                         relevant in the 21th century",
+                "description": "Coming Soon!",
+                "id": "2"
+            },
+            {
+                "title": "Columbian Exchange",
+                "image": "barge",
+                "progress": "20",
+                "step": "aha",
+                "hook": "The quick and enjoyable way to cross the Columbia \
+                        River Gorge",
+                "description": "Coming Soon!",
+                "id": "3"
+            },
+            {
+                "title": "Boston Express",
+                "image": "boat",
+                "progress": "30",
+                "step": "gap",
+                "hook": "Want to captain your own ship through Boston Harbor \
+                        try Boston express rent a boat!",
+                "description": "Coming Soon!",
+                "id": "4"
+            },
+            {
+                "title": "Bricks to Buildings",
+                "image": "bricks",
+                "progress": "50",
+                "step": "survey",
+                "hook": "We take ruble from demolitions and make low cost \
+                         housing",
+                "description": "Coming Soon!",
+                "id": "5"
+            },
+            {
+                "title": "Sands of Time",
+                "image": "sand",
+                "progress": "90",
+                "step": "end",
+                "hook": "Sand is ever flowing and timeless",
+                "description": "Coming Soon!",
+                "id": "6"
+            },
+            {
+                "title": "Turtle Bay",
+                "image": "sunglasses",
+                "progress": "30",
+                "step": "aha",
+                "hook": "Sunglasses that can be worn in the sea",
+                "description": "Coming Soon!",
+                "id": "7"
+            },
+            {
+                "title": "Hawaiian Reef Tours",
+                "image": "turtle",
+                "progress": "50",
+                "step": "survey",
+                "hook": "We have scouted the best reefs on Hawaii \
+                        and want to give you the experiance of a lifetime",
+                "description": "Coming Soon!",
+                "id": "8"
             }
         ]
-
-        currentVenture = {
-            "image": "glyphicon-briefcase",
-            "title": "Current Venture",
-            "description": "You're doing good! Keep it up!"
-        }
 
         return render(
             request,
             'members.html',
             dict(
-                news=news,
-                currentVenture=currentVenture
+                ventures=ventures[1:],
+                currentVenture=ventures[0]
             )
         )
     else:
