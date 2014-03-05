daced5de269a89e0ff65dc6615dd83d047b496a2
Becca Little <rebecca.little@asu.edu>
2014-03-05 13:01:10 -0700

    hopefully fixed travis CI fail

--- static/js/Models/robotDude.js
+++ static/js/Models/robotDude.js
@@ -12,7 +12,7 @@ function init() {
 	var canvasHeight = window.innerHeight;
 
 	// CAMERA
-	camera = new THREE.PerspectiveCamera( 45,   	window.innerWidth / window.innerHeight, .1, 20000 );
+	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 20000 );
 	camera.position.set( 0, 2, 10 );
 	camera.lookAt(0,0,0);
 
