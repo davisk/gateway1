dd2c4b27f26f0fa12048148cd42219667643c015
Christian Murphy <christian.murphy@asu.edu>
2014-03-04 23:08:52 -0700

    first three ventures

--- controllers/venture.py
+++ controllers/venture.py
@@ -6,10 +6,30 @@ def venture_view(request, id="0"):
     ventures = [
         {
             "title": "Murphee's Irish Coats",
-            "progress": "30",
+            "image": "coat",
+            "progress": "90",
+            "step": "elevator",
             "hook": "Bringing the comfort and quality of Traditional Irish\
                     coats to an Online International Exchange",
-            "description": ""
+            "description": "test"
+        },
+        {
+            "title": "Three Lions Magic Show",
+            "image": "anti-gravity",
+            "progress": "60",
+            "step": "survey",
+            "hook": "A crew of three globally acclaimed Magicians bend the \
+                    rules of reality to make the mundane extraordinary",
+            "description": "test"
+        },
+        {
+            "title": "Melies Studio",
+            "image": "film",
+            "progress": "70",
+            "step": "interest",
+            "hook": "Georges Melies makes Black and White cinematography \
+                     relevant in the 21th century",
+            "description": "test"
         }
     ]
 
