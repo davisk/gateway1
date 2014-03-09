3662be4790552d7ebb3cda05726671221782b2c0
developer <Joe.Stratton@asu.edu>
2014-03-09 15:24:19 -0700

    final registration

--- models/models.py
+++ models/models.py
@@ -1,10 +1,3 @@
 from django.db import models
-from django.dispatch import receiver
-from registration.signals import user_activated
-from django.contrib.auth import login
 
-@receiver(user_activated)
-def login_on_activation(sender, user, request, **kwargs):
-    """Logs in the user after activation"""
-    user.backend = 'django.contrib.auth.backends.ModelBackend'
-    login(request, user)
+#put models here
