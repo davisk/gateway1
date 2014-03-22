23a84e82d988a4a3f83ce747cb70d875ea894e7f
Christian Murphy <christian.murphy@asu.edu>
2014-03-22 08:51:43 -0700

    style fix in test suite

--- gateway/tests/__init__.py
+++ gateway/tests/__init__.py
@@ -1,5 +1,7 @@
 """manage test imports."""
 import unittest
 
-def suite():   
+
+def suite():
+    """collect and run all tests for gateway."""
     return unittest.TestLoader().discover("gateway.tests", pattern="*.py")
