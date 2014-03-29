0941db9b81d5f91090e1a67938842519962700bf
Davis Kendry <davis.kendry@gmail.com>
2014-03-29 14:47:02 -0700

    venture.py

---
+++ gateway/models/Aha.py
@@ -0,0 +1,23 @@
+import models
+
+"""model for the Aha minigame"""
+
+
+class Aha(models.Model):
+
+    is_complete = models.BooleanField()
+
+    question_one = models.IntegerField()
+
+    question_two = models.IntegerField()
+
+    question_three = models.IntegerField()
+
+    question_four = models.IntegerField()
+
+    question_five = models.IntegerField()
+
+    question_six = models.IntegerField()
+
+    class Meta:
+        app_label = 'gateway'
