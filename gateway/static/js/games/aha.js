ad58803c453ef1f4dd7c1a93f10b5a5ec4630d25
sl8tr <bsleater@asu.edu>
2014-04-16 16:59:37 -0700

    More interaction with the user answers for game aha

--- gateway/static/js/games/aha.js
+++ gateway/static/js/games/aha.js
@@ -2,7 +2,11 @@
     These are specific functions that DIFFER between games
 */
 
-var txtbox, txtbox_words;
+var txtbox, answer_txtbx;
+
+var dialog = [];
+
+var answers = [];
 
 var goToNextGame = false;
 
@@ -15,8 +19,21 @@ function initImages() {
     manifest = [
         {id: "canvas_bkgd", src: "/static/sprites/scene2_init.png"},
         {id: "game_guide", src: "/static/sprites/Main_guy.png"},
-        {id: "welcome_txtbox", src: "/static/sprites/Textbox.png"},
-        {id: "welcome_txt", src: "/static/sprites/s1_text/s1_1.png"}
+        {id: "welcome_txtbox", src: "/static/sprites/Textbox2.png"},
+        {id: "welcome_txt1", src: "/static/sprites/s2_text/s2_1.png"},
+        {id: "welcome_txt2", src: "/static/sprites/s2_text/s2_2.png"},
+        {id: "welcome_txt3", src: "/static/sprites/s2_text/s2_3.png"},
+        {id: "welcome_txt4", src: "/static/sprites/s2_text/s2_4.png"},
+        {id: "welcome_ans1", src: "/static/sprites/s2_text/s2_a1.png"},
+        {id: "welcome_ans2", src: "/static/sprites/s2_text/s2_a2.png"},
+        {id: "welcome_ans3", src: "/static/sprites/s2_text/s2_a3.png"},
+        {id: "welcome_ans4", src: "/static/sprites/s2_text/s2_a4.png"},
+        {id: "welcome_ans5", src: "/static/sprites/s2_text/s2_a5.png"},
+        {id: "welcome_ans6", src: "/static/sprites/s2_text/s2_a6.png"},
+        {id: "welcome_ans7", src: "/static/sprites/s2_text/s2_a7.png"},
+        {id: "welcome_ans8", src: "/static/sprites/s2_text/s2_a8.png"},
+        {id: "welcome_ans9", src: "/static/sprites/s2_text/s2_a9.png"},
+        {id: "answer_txtbx", src: "/static/sprites/Textbox.png"}
     ];
 }
 
@@ -37,7 +54,16 @@ function loadGameConfig() {
     imgs[1].scaleX = 0.8;
 
     txtbox = imgs[2];
-    txtbox_words = imgs[3];
+
+    for (i = 3; i < 7; i++) {
+        dialog.push(imgs[i]);
+    }
+
+    for (i = 7; i < 10; i++) {
+        answers.push(imgs[i]);
+    }
+
+    answer_txtbx = imgs[10];
 
     // Add our images and remove progress bar
     for (i = 0; i < 2; i++) stage.addChild(imgs[i]);
@@ -55,19 +81,66 @@ function getStartingPlayerConfig() {
 
 function initInteraction() {
 
-    txtbox.x = 200;
-    txtbox.y = 475;
+    txtbox.x = 950;
+    txtbox.y = 50;
     txtbox.scaleY = 0.5;
-    txtbox.scaleX = 1;
+    txtbox.scaleX = 0.8;
 
-    txtbox_words.x = 215;
-    txtbox_words.y = 475;
-    txtbox_words.scaleY = 0.7;
-    txtbox_words.scaleX = 1;
+    answer_txtbx.x = 170;
+    answer_txtbx.y = 440;
+
+    answer_txtbx.scaleX = 1.06;
+    answer_txtbx.scaleY = 0.55;
+
+    for (i = 0; i < dialog.length; i++) {
+        dialog[i].x = 950;
+        dialog[i].y = 50;
+        dialog[i].scaleY = 0.6;
+        dialog[i].scaleX = 0.8;
+
+        // TODO: Find out why this isn't working..
+        /*dialog[i].addEventListener("click", function(event) {
+            stage.removeChild(dialog[i]);
+            stage.addChild(dialog[i+1]);
+        });*/
+    }
+
+    answers[0].x = 180;
+    answers[1].x = 300;
+    answers[2].x = 425;
+
+    for (i = 0; i < answers.length; i++) {
+        answers[i].y = 440;
+        answers[i].scaleY = 0.6;
+        answers[i].scaleX = 0.8;
+    }
+
+    answers[0].
+
+    dialog[0].addEventListener("click", function(event) {
+        stage.removeChild(dialog[0]);
+        stage.addChild(dialog[1]);
+    });
+
+    dialog[1].addEventListener("click", function(event) {
+        stage.removeChild(dialog[1]);
+        stage.addChild(dialog[2]);
+    });
+
+    dialog[2].addEventListener("click", function(event) {
+        stage.removeChild(dialog[2]);
+        stage.addChild(dialog[3]);
+
+        stage.addChild(answer_txtbx);
+
+        for (i = 0; i < answers.length; i++) {
+            stage.addChild(answers[i]);
+        }
+    });
 
     goToNextGame = true;
 
-    stage.addChild(txtbox, txtbox_words);
+    stage.addChild(txtbox, dialog[0]);
     stage.update();
 }
 
