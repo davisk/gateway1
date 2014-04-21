548c78eac51733663e34d17ffda4a1a07cbd7c99
Davis Kendry <davis.kendry@gmail.com>
2014-04-21 14:58:26 -0700

    Gap.py

---
+++ gateway/models/Interest.py
@@ -0,0 +1,39 @@
+"""Start minigame information."""
+"""Will change based on number of questions"""
+from django.db import models
+
+
+class Interest(models.Model):
+
+    """store Minigame Game Data for Interest game"""
+
+    is_complete = models.BooleanField(
+        "The model Completion status of the Interest game"
+    )
+
+    question_one = models.IntegerField(
+        "The first question of the Interest game"
+    )
+
+    question_two = models.IntegerField(
+        "The second question of teh Interest game"
+    )
+
+    question_three = models.IntegerField(
+        "The third question of the Interest game"
+    )
+
+    question_four = models.IntegerField(
+        "The fourth question of the Interest game"
+    )
+
+    question_five = models.IntegerField(
+        "the fifth question of the Interest game"
+    )
+
+    question_six = models.IntegerField(
+        "the sixth question of the Interest game"
+    )
+
+    class Meta:
+        app_label = 'gateway'
