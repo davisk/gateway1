843b91c061514c65a722457eb48f589ae86e2152
Justin <jed833@gmail.com>
2014-04-21 21:57:27 -0700

    update repo

--- gateway/static/js/games/interest.js
+++ gateway/static/js/games/interest.js
@@ -3,6 +3,9 @@
 */
 
 
+var txtbox;
+var goToNextGame = false;
+
 function getCanvasId() {
     return "interCanvas";
 }
@@ -11,29 +14,35 @@ function initImages() {
 
     manifest = [
         {id: "canvas_bkgd", src: "/static/sprites/scene5_init.png"},
-        {id: "game_npc", src: "/static/sprites/Dash_2.png"}
+        {id: "game_npc", src: "/static/sprites/Dash_2.png"},
+        {id: "int_txtbox", src: "/static/sprites/Textbox.png"}
     ];
 }
 
 function loadGameConfig() {
 
     var imgs = [];
-    var catch = 0;
-    var timeoutID;
 
     for (i = 0; i < manifest.length; i++) imgs[i] = new createjs.Bitmap(preload.getResult(manifest[i].id));
     
 
     // Fix the background image for the canvas
     imgs[0].scaleY = 0.6;
+    
+    //Setup scale the bro
+    
+    imgs[1].x = 450;
+    imgs[1].y = 200;
     imgs[1].scaleX = 0.8;
     imgs[1].scaleY = 0.8;
-    //Setup scale the bro
+
+    txtbox = imgs[2];
+    //txtbox = imgs[3];
+
 
     // Add our images to the canvas and remove the progress bar
     for (i = 0; i < 2; i++) stage.addChild(imgs[i]);
 
-    imgs[1].
 
 }
 
@@ -47,4 +56,21 @@ function getStartingPlayerConfig() {
     };
 }
 
+function initInteraction() {
+
+    txtbox.x = 200;
+    txtbox.y = 475;
+    txtbox.scaleX = 1;
+    txtbox.scaleY = 0.5;
+
+    goToNextGame = true;
+
+    stage.addChild(txtbox);
+    stage.update();
+
+}
+
+function switchGame() {
+    window.location = window.location.protocol + "//" + window.location.host + "/" + "minigame/elevator";
+}
 
